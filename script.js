ef5d662d9f2ef4f5e9c35058d70479592ee04929
Nata <melvia.nr@gmail.com>
2022-10-04 21:24:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var spotlight = document.querySelector(".spotlight");
+var spotlightSize = "transparent 160px, rgba(0, 0, 0, 0.95) 200px)";
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("mousemove", (e) => updateSpotlight(e));
+
+  function updateSpotlight(e) {
+    spotlight.style.backgroundImage = `radial-gradient(circle at ${
+      (e.pageX / window.innerWidth) * 100
+    }% ${(e.pageY / window.innerHeight) * 100}%, ${spotlightSize}`;
+  }
+});
