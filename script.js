7441e2e9dc669c316bbac857e98a9ddc636f0911
Nata <melvia.nr@gmail.com>
2022-10-04 21:27:40 +0500

    spotlight effect

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var spotlight = document.querySelector(".spotlight");
-var spotlightSize = "transparent 160px, rgba(0, 0, 0, 0.95) 200px)";
+let spotlight = document.querySelector(".spotlight");
+let spotlightSize = "transparent 160px, rgba(0, 0, 0, 0.95) 200px)";
 
-window.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("mousemove", (e) => updateSpotlight(e));
+window.addEventListener("mousemove", (e) => updateSpotlight(e));
 
-  function updateSpotlight(e) {
-    spotlight.style.backgroundImage = `radial-gradient(circle at ${
-      (e.pageX / window.innerWidth) * 100
-    }% ${(e.pageY / window.innerHeight) * 100}%, ${spotlightSize}`;
-  }
-});
+function updateSpotlight(e) {
+  spotlight.style.backgroundImage = `radial-gradient(circle at ${
+    (e.pageX / window.innerWidth) * 100
+  }% ${(e.pageY / window.innerHeight) * 100}%, ${spotlightSize}`;
+}
